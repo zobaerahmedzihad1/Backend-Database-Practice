038cc8cea26ba4169d04d116375c34771e05b938
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2022-05-12 18:05:45 +0600

    create backend dynamic route

--- basic-crud-operation/index.js
+++ basic-crud-operation/index.js
@@ -2,8 +2,24 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const users = [
+  { id: "1", name: "zihad" },
+  { id: "2", name: "jewel" },
+  { id: "3", name: "sahin" },
+  { id: "4", name: "masum" },
+  { id: "5", name: "shiplu" },
+  { id: "6", name: "bellal" },
+];
 app.get("/", (req, res) => {
   res.send("Hey mama I'm from server.");
+});
+
+app.get("/users/:id", (req, res) => {
+  console.log(req.params);
+  const id = req.params.id;
+  //   const user = users[id];
+  const user = users.find((user) => user.id === id);
+  res.send(user);
 });
 
 app.listen(port, () => {
