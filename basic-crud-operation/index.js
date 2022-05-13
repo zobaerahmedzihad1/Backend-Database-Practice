a7391663ffae7c00674cdd826776bf2231209c41
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2022-05-13 15:02:53 +0600

    Display POST data on the UI and explore query parameter

--- basic-crud-operation/index.js
+++ basic-crud-operation/index.js
@@ -1,24 +1,44 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
 
 const users = [
   { id: "1", name: "zihad" },
   { id: "2", name: "jewel" },
-  { id: "3", name: "sahin" },
-  { id: "4", name: "masum" },
+  { id: "3", name: "md sahin" },
+  { id: "4", name: "md masum" },
   { id: "5", name: "shiplu" },
   { id: "6", name: "bellal" },
 ];
-app.get("/", (req, res) => {
-  res.send("Hey mama I'm from server.");
+app.get("/users", (req, res) => {
+  // console.log('query ',req.query);
+  // filter by query parameter.
+  if (req.query.name) {
+    const search = req.query.name.toLocaleLowerCase();
+    const matched = users.filter((user) =>
+      user.name.toLocaleLowerCase().includes(search)
+    );
+    res.setTimeout(matched);
+  } else {
+    res.send(users);
+  }
+});
+
+app.post("/user", (req, res) => {
+  // console.log(req.body);
+  const user = req.body;
+  user.id = users.length + 1;
+  users.push(user);
+  res.send(user);
 });
 
 app.get("/users/:id", (req, res) => {
-  console.log(req.params);
   const id = req.params.id;
-  //   const user = users[id];
-  const user = users.find((user) => user.id === id);
+  const user = users.find((user) => user.id == id);
   res.send(user);
 });
 
