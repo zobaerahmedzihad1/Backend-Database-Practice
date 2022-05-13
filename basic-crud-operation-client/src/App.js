1203c6d99c20a31af5cfff560f91f21e4a899b64
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2022-05-13 15:04:16 +0600

    send data to server usuing feacth post method.

--- basic-crud-operation-client/src/App.js
+++ basic-crud-operation-client/src/App.js
@@ -3,6 +3,28 @@ import "./App.css";
 
 function App() {
   const [users, setUsers] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = { name, email };
+
+    fetch("http://localhost:5000/user", {
+      method: "post",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        const newUser = [...users, data];
+        setUsers(newUser);
+      });
+  };
+
   useEffect(() => {
     fetch(`http://localhost:5000/users`)
       .then((response) => response.json())
@@ -10,6 +32,15 @@ function App() {
   }, []);
   return (
     <div className="App">
+      <h3>send data to server.</h3>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" id="" required placeholder="Name" />
+        <br />
+        <input type="email" name="email" id="" required placeholder="Email" />
+        <br />
+        <input type="submit" value="submit" />
+      </form>
+
       <h3>Users : {users.length} </h3>
       <ul>
         {users.map((user) => (
