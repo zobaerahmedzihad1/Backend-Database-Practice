06da6e74b18064606160973a1495e520690604a9
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2022-05-14 12:58:40 +0600

    Send data to database using mongodb insertOne

--- node-mongo-crud-server/index.js
+++ node-mongo-crud-server/index.js
@@ -1,28 +1,34 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-// username : dbuser
-// password : Fb113kX58zQez04A
+// middleware
+app.use(cors());
+app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri =
-  "mongodb+srv://process.env.DATABASE_USER:process.env.DATABASE_PASSWORD@cluster0.rb00t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "mongodb+srv://dbuser:Fb113kX58zQez04A@cluster0.rb00t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  console.log("connected WITH dot env.");
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
-app.use(cors());
-app.use(express.json());
+async function run() {
+  try {
+    await client.connect();
+    const userCollection = client.db("foodExpress").collection("user");
+
+    const user = { name: "karima BRO", age: "25" };
+    const result = await userCollection.insertOne(user);
+    console.log(`Send data to database: ${result.insertedId}`);
+  } finally {
+    // await client.close()
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Server is ready.");
