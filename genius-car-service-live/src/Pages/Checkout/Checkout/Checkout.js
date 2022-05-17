863a8fda53b23feaa7f619da74806254795be01c
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2022-05-17 11:54:20 +0600

    Get Order data, create API endpoint and readonly input

--- genius-car-service-live/src/Pages/Checkout/Checkout/Checkout.js
+++ genius-car-service-live/src/Pages/Checkout/Checkout/Checkout.js
@@ -1,35 +1,53 @@
-import React, { useState } from "react";
+import React from "react";
 import { useParams } from "react-router-dom";
 import useServiceDetail from "../../../hooks/useServiceDetail";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
 
 const Checkout = () => {
   const { serviceId } = useParams();
   const [service, setService] = useServiceDetail(serviceId);
-  const [user, setUser] = useState({
-    name: "zihad bro",
-    email: "zihad@gmail.com",
-    address: "Dhaka",
-    phone: "012354",
-  });
+  const [user] = useAuthState(auth);
 
-  const handleChangeAddress = (event) => {
-    const { address, ...rest } = user;
-    const newAddress = event.target.value;
-    const newUser = { address: newAddress, ...rest };
-    setUser(newUser);
-    console.log(newUser);
+  const handleOrderSubmit = (event) => {
+    event.preventDefault();
+
+    const order = {
+      service: service.name,
+      name: user.displayName,
+      email: user.email,
+      phone: event.target.phone.value,
+      address: event.target.address.value,
+    };
+    console.log(order);
   };
+
+  // const [user, setUser] = useState({
+  //   name: "zihad bro",
+  //   email: "zihad@gmail.com",
+  //   address: "Dhaka",
+  //   phone: "012354",
+  // });
+
+  // const handleChangeAddress = (event) => {
+  //   const { address, ...rest } = user;
+  //   const newAddress = event.target.value;
+  //   const newUser = { address: newAddress, ...rest };
+  //   setUser(newUser);
+  //   console.log(newUser);
+  // };
   return (
     <div className="text-center">
       <h2>Please Checkout your booking</h2>
       <h3>Service Name : {service.name} </h3>
-      <form>
+      <form onSubmit={handleOrderSubmit}>
         <input
           className="w-50 mb-2"
           type="text"
           name="name"
-          value={user.name}
+          value={user.displayName}
           placeholder="name"
+          disabled
         />
         <br />
         <input
@@ -38,6 +56,7 @@ const Checkout = () => {
           name="email"
           value={user.email}
           placeholder="email"
+          disabled
         />
         <br />
         <input
@@ -46,23 +65,23 @@ const Checkout = () => {
           name="service"
           value={service.name}
           placeholder="service"
+          disabled
         />
         <br />
         <input
           className="w-50 mb-2"
-          type="text"
+          type="number"
           name="phone"
-          value={user.phone}
           placeholder="phone"
+          autoComplete="off"
         />
         <br />
         <input
           className="w-50 mb-2"
           type="text"
           name="address"
-          onChange={handleChangeAddress}
-          value={user.address}
           placeholder="address"
+          autoComplete="off"
         />
         <br />
         <input className="w-50" type="submit" value="Place Order" />
