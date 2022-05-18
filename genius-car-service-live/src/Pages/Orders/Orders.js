5322519a131104b0c99a9960c2a13fa33237e7a2
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2022-05-18 22:42:02 +0600

    Handle 401, 403 to log out user and interceptors

--- genius-car-service-live/src/Pages/Orders/Orders.js
+++ genius-car-service-live/src/Pages/Orders/Orders.js
@@ -1,22 +1,34 @@
 import axios from "axios";
+import { signOut } from "firebase/auth";
 import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
+import axiosPrivate from "../../api/axiousPrivate";
 import auth from "../../firebase.init";
 
 const Orders = () => {
   const [orders, setOrders] = useState([]);
   const [user] = useAuthState(auth);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const getOrders = async () => {
       const email = user.email;
       const url = `http://localhost:5000/order?email=${email}`;
-      const { data } = await axios.get(url, {
-        headers: {
-          authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-        },
-      });
-      setOrders(data);
+      try {
+        const { data } = await axiosPrivate.get(url);
+        //   headers: {
+        //     authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+        //   },
+        // });
+        setOrders(data);
+      } catch (error) {
+        console.log(error);
+        if (error.response.status === 401 || error.response.status === 403) {
+          navigate("/login");
+          signOut(auth);
+        }
+      }
     };
     getOrders();
   }, [user]);
