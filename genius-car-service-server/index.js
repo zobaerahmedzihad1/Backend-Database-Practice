a95fc84afe9fd74bca5a639b45457ebec4384dce
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2022-05-18 22:43:45 +0600

    Handle 401, 403 to log out user and interceptors

--- genius-car-service-server/index.js
+++ genius-car-service-server/index.js
@@ -15,20 +15,38 @@ app.use(express.json());
 // verify jwt
 function verifyJWT(req, res, next) {
   const authHeader = req.headers.authorization;
+  // console.log(authHeader);
   if (!authHeader) {
-    return res.status(401).send({ message: "Unauthorized access ." });
+    res.status(401).send({ message: "Unauthorized access." });
   }
   const token = authHeader.split(" ")[1];
-  // console.log("only token", token);
-  jwt.verify(token, process.env.SECRET_ACCESS_TOKEN, (err, decoded) => {
+  // console.log(token);
+  jwt.verify(token, process.env.SECRET_ACCESS_TOKEN, (err, decode) => {
     if (err) {
-      return res.status(403).send({ message: "Forbidden access." });
+      res.status(403).send({ message: "Forbidden access." });
     }
-    // console.log(decoded, 'decoded');
-    req.decoded = decoded;
+    req.decode = decode;
+    // console.log(decode);
     next();
   });
 }
+
+// function verifyJWT(req, res, next) {
+//   const authHeader = req.headers.authorization;
+//   if (!authHeader) {
+//     return res.status(401).send({ message: "Unauthorized access ." });
+//   }
+//   const token = authHeader.split(" ")[1];
+//   // console.log("only token", token);
+//   jwt.verify(token, process.env.SECRET_ACCESS_TOKEN, (err, decoded) => {
+//     if (err) {
+//       return res.status(403).send({ message: "Forbidden access." });
+//     }
+//     // console.log(decoded, 'decoded');
+//     req.decoded = decoded;
+//     next();
+//   });
+// }
 
 const uri = `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.rb00t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -90,17 +108,14 @@ async function run() {
     //  order collection api
     // get orders data
     app.get("/order", verifyJWT, async (req, res) => {
-      const decodedEmail = req.decoded.email;
-      const email = req.query.email;
+      const decodedEmail = req.decode.email;
       // console.log(decodedEmail);
-
+      const email = req.query.email;
       if (decodedEmail === email) {
         const query = { email: email };
         const cursor = orderCollection.find(query);
         const result = await cursor.toArray();
         res.send(result);
-      } else {
-        res.status(403).send({ message: "Forbidden access." });
       }
     });
 
